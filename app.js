4755123a9a92cbf24aadfbb9840f008f15828630
matthias <matthiaspark0514@gmail.com>
2021-06-14 20:44:43 -0700

    add nosql

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const feedRoutes = require('./routes/feed');
+const feedRoutes = require("./routes/feed");
 
-const { use } = require('./routes/feed');
+const { use } = require("./routes/feed");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,12 +12,24 @@ const app = express();
 app.use(bodyParser.json()); //application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-})
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
-app.use('/feed', feedRoutes);
+app.use("/feed", feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(
+    "mongodb+srv://matthias:password@node-complete.cqski.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- routes/feed.js
+++ routes/feed.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const { body } = require('express-validator/check');
 
 const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
-// Get /feed/posts
+// GET /feed/posts
 router.get('/posts', feedController.getPosts);
 
-router.post('/post', feedController.createPost);
+// POST /feed/post
+router.post(
+  '/post',
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.createPost
+);
 
 module.exports = router;
