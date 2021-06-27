7865c2a443070abf63e1520dd22ae339e1832ebd
matthias <matthiaspark0514@gmail.com>
2021-06-27 14:14:24 -0700

    auth processing

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,81 @@
-const {validationResult} = require('express-validator/check');
+const { validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const User = require('../models/user');
-
+const User = require("../models/user");
 
 exports.singup = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        const error = new Error('Validation Failed');
-        error.statusCode = 422;
-        error.data = errors.array();
-        throw error;
-    }
-    const email = req.body.email;
-    const name = req.body.name;
-    const password = req.body.password;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation Failed");
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  const email = req.body.email;
+  const name = req.body.name;
+  const password = req.body.password;
+  bcrypt
+    .hash(password, 12)
+    .then((hasedPw) => {
+      const user = new User({
+        email: email,
+        password: hasedPw,
+        name: name,
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.status(201).json({
+        message: "User Created",
+        userId: result._id,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
 
-}
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error("A user with this email is not found");
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      bcrypt.compare(password, user.password);
+    })
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error("Wrong Passwrod");
+        error.statusCode = 401;
+        throw error;
+      }
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString(),
+        },
+        "somesupersecretsecret",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({
+        token: token,
+        userId: loadedUser._id.toString(),
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
