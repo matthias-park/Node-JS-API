e76bcca38e08c8497fb537428ff4b5a5de9a5fc6
matthias <matthiaspark0514@gmail.com>
2021-07-29 14:14:09 -0700

    update, delete post and fix pagination bug

--- app.js
+++ app.js
@@ -7,26 +7,27 @@ const mongoose = require("mongoose");
 const multer = require("multer");
 const { graphqlHTTP } = require("express-graphql");
 
-const graphqlSchme = require("./graphql/Schema");
-const graphqlResolvers = require("./graphql/resolvers");
-const auth = require('./middleware/auth')
+const graphqlSchema = require("./graphql/Schema");
+const graphqlResolver = require("./graphql/resolvers");
+const auth = require('./middleware/auth');
+const {clearImage } = require('./util/file');
 
 const app = express();
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "images");
+    cb(null, 'images');
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + "-" + file.originalname);
-  },
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
 });
 
 const fileFilter = (req, file, cb) => {
   if (
-    file.mimetype === "image/png" ||
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/jpeg"
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
   ) {
     cb(null, true);
   } else {
@@ -37,18 +38,18 @@ const fileFilter = (req, file, cb) => {
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 );
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if(req.method === 'OPTIONS') {
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
   next();
@@ -57,32 +58,34 @@ app.use((req, res, next) => {
 app.use(auth);
 
 app.put('/post-image', (req, res, next) => {
-  if(!req.isAuth) {
-    throw new Error('Not Authenticated');
+  if (!req.isAuth) {
+    throw new Error('Not authenticated!');
   }
-  if(!req.file) {
-    return res.status(200).json({message: 'No file provided'});
+  if (!req.file) {
+    return res.status(200).json({ message: 'No file provided!' });
   }
   if (req.body.oldPath) {
     clearImage(req.body.oldPath);
   }
-  return res.status(201).json({message: 'file stored', filePath: req.file.path})
+  return res
+    .status(201)
+    .json({ message: 'File stored.', filePath: req.file.path });
 });
 
 app.use(
-  "/graphql",
+  '/graphql',
   graphqlHTTP({
-    schema: graphqlSchme,
-    rootValue: graphqlResolvers,
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
     graphiql: true,
     formatError(err) {
-      if(err.originalError) {
+      if (!err.originalError) {
         return err;
       }
       const data = err.originalError.data;
-      const message = err.message || 'An Error occoured';
+      const message = err.message || 'An error occurred.';
       const code = err.originalError.code || 500;
-      return {message: message, status: code, data: data};
+      return { message: message, status: code, data: data };
     }
   })
 );
@@ -95,6 +98,7 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message: message, data: data });
 });
 
+
 mongoose
   .connect(
     "mongodb+srv://matthias:password@node-complete.cqski.mongodb.net/message?retryWrites=true&w=majority"
@@ -104,7 +108,3 @@ mongoose
   })
   .catch((err) => console.log(err));
 
-  const clearImage = filePath => {
-    filePath = path.join(__dirname, '..', filePath);
-    fs.unlink(filePath, err => console.log(err));
-  };
